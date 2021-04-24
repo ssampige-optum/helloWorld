7f4976195927c8a61ce4b29e3d02b922724ecf48
ssampige-optum <srinivas.sampige@optum.com>
2021-04-24 19:35:34 +0000

    Mofidiled pom.xml, and added AADSecurityConfig as per https://github.com/Azure/azure-sdk-for-java/blob/azure-spring-boot-starter-active-directory_3.4.0/sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-active-directory-resource-server/pom.xml

---
+++ src/main/java/org/springframework/samples/petclinic/security/AADSecurityConfig.java
@@ -0,0 +1,22 @@
+package org.springframework.samples.petclinic.security;
+
+import com.azure.spring.aad.webapi.AADResourceServerWebSecurityConfigurerAdapter;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AADSecurityConfig extends AADResourceServerWebSecurityConfigurerAdapter
+{
+    /**
+     * Add configuration logic as needed.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+
+}
