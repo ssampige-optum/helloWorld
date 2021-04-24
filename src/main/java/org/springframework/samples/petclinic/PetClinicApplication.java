4bd94c2d48281f052bfdf83fc55190ff90381dbd
ssampige-optum <srinivas.sampige@optum.com>
2021-04-24 20:43:27 +0000

    disabled security

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -29,6 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
     public static void main(String[] args) {
+        System.out.println("@@@@@@@@@@@ starting application");
         SpringApplication.run(PetClinicApplication.class, args);
     }
 
--- src/main/java/org/springframework/samples/petclinic/security/AADSecurityConfig.java
+++ src/main/java/org/springframework/samples/petclinic/security/AADSecurityConfig.java
@@ -2,18 +2,18 @@ package org.springframework.samples.petclinic.security;
 
 import com.azure.spring.aad.webapi.AADResourceServerWebSecurityConfigurerAdapter;
 
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+/*import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;*/
 
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AADSecurityConfig extends AADResourceServerWebSecurityConfigurerAdapter
 {
     /**
      * Add configuration logic as needed.
      */
-    @Override
+    /*@Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.authorizeRequests().anyRequest().authenticated();
@@ -21,6 +21,6 @@ public class AADSecurityConfig extends AADResourceServerWebSecurityConfigurerAda
         {
             System.out.println(" ------------------------------------->>>> FINISHED Configuring AADSecurityConfig !!!!!");
         }
-    }
+    }*/
 
 }
