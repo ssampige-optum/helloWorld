23a3117fb3ccf66641ebd4fc8bffcd891b5497dc
ssampige <ssampige@LHTU0CND6280945.ms.ds.uhc.com>
2021-04-27 15:09:13 -0700

    uncommented @EnableWebSecurity

--- src/main/java/com/aad/demo/AADSecurityConfig.java
+++ src/main/java/com/aad/demo/AADSecurityConfig.java
@@ -2,25 +2,26 @@ package com.aad.demo;
 
 import com.azure.spring.aad.webapi.AADResourceServerWebSecurityConfigurerAdapter;
 
-/*import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;*/
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-//@EnableWebSecurity
+@EnableWebSecurity
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AADSecurityConfig extends AADResourceServerWebSecurityConfigurerAdapter
 {
     /**
      * Add configuration logic as needed.
      */
-    /*@Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.authorizeRequests().anyRequest().authenticated();
-        for(int i=0;i<10;i++)
+        for(int i=0;i<20;i++)
         {
-            System.out.println(" ------------------------------------->>>> FINISHED Configuring AADSecurityConfig !!!!!");
+            System.out.println(i+" ------------------------------------->>>> FINISHED Configuring AADSecurityConfig !!!!!");
         }
-    }*/
+        System.out.println("END  ============== END");
+    }
 
 }
