6f4f412efaa219bb5fa360d36e8768d7adaf805b
ssampige <ssampige@LHTU0CND6280945.ms.ds.uhc.com>
2021-04-27 16:33:52 -0700

    added logger. changed banner.

--- src/main/java/com/aad/demo/AzureADOAuth2ResourceServerSampleApplication.java
+++ src/main/java/com/aad/demo/AzureADOAuth2ResourceServerSampleApplication.java
@@ -1,19 +1,28 @@
 package com.aad.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AzureADOAuth2ResourceServerSampleApplication {
 
+
+@SpringBootApplication
+public class AzureADOAuth2ResourceServerSampleApplication 
+{
+	private static Logger logger = LoggerFactory.getLogger(AzureADOAuth2ResourceServerSampleApplication.class);
+	
     public static void main(String[] args) 
     {
+    	logger.info("############# main() ###############");
     	System.out.println("############# main() ###############");
         SpringApplication.run(AzureADOAuth2ResourceServerSampleApplication.class, args);
         for (int i=0;i<5;i++) 
         {
         	System.out.println("V3 Starting application......"+i);
+        	logger.info("V3 Starting application......"+i);
         }
         System.out.println("----------- INITIALIZED APPLICATION----------------------");
+        logger.info("----------- INITIALIZED APPLICATION----------------------");
     }
 }
